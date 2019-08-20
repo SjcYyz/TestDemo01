7aa5483ca28bf2551366723125209217baa211a4
SjcYyz <thbshenjc@tedu.cn>
2019-08-20 11:50:49 +0800

    新的测试类

---
+++ GitTest/src/cn/tedu/com/Demo1.java
@@ -0,0 +1,17 @@
+package cn.tedu.com;
+/**
+ *  本类用于测试使用
+ * @author Sjc
+ *
+ */
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("我是中国人");
+		
+		
+		
+	}
+
+}

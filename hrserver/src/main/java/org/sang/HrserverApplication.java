26cc1f318dc7df447a368abaac89fdfa5f68f368
SjcYyz <thbshenjc@tedu.cn>
2019-08-20 12:07:23 +0800

    新的启动类注释

--- hrserver/src/main/java/org/sang/HrserverApplication.java
+++ hrserver/src/main/java/org/sang/HrserverApplication.java
@@ -4,8 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-
+/**
+ * 启动类
+ * @author Sjc
+ *
+ */
 @SpringBootApplication
+//包扫描
 @MapperScan("org.sang.mapper")
 @EnableCaching
 public class HrserverApplication {
